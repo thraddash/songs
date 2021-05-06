b8ffe4d2c050a3ba8460d9ec0924902eba027d4a
thraddash <theartist@gmail.com>
2021-05-06 00:11:46 -0700

    fix songsReducer typo, add selected song reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,16 @@
-const songsREducer = () => {
+const songsReducer = () => {
     return [
         { title: 'Hey Brother', duration: '4:23' },
         { title: 'Feel So Close (Nero Remix)', duration: '4:44' },
         { title: 'The Nights', duration: '3:10' },
         { title: 'Waiting For Love', duration: '3:10' }
     ];
+};
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    
+    return selectedSong;
 };
