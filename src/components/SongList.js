0085137b19533e856f160162bbd456e027c20354
thraddash <theartist@gmail.com>
2021-05-06 16:08:04 -0700

    add semantic ui

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,10 +3,26 @@ import { connect } from "react-redux";
 
 // class SongList extends React.Component
 class SongList extends Component {
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        });
+    }
+
+
     render() {
         // this.props === { songs: state.songs }
         console.log(this.props);
-        return <div>SongList</div>;
+        return <div className="ui divided list">{this.renderList()}</div>;
     }
 }
 
