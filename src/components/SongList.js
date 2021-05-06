63ae19bb47428f9ce3d1ac6a35822efa5626fb5b
thraddash <theartist@gmail.com>
2021-05-06 01:23:40 -0700

    use redux way of geting data to props

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,15 +4,17 @@ import { connect } from "react-redux";
 // class SongList extends React.Component
 class SongList extends Component {
     render() {
+        // this.props === { songs: state.songs }
+        console.log(this.props);
         return <div>SongList</div>;
     }
 }
 
 // get state from Store, pass to mapStateToProp function to connect component
 const mapStateToProps = (state) => {
-    console.log(state);
 
-    return state;
+    //redux way of getting data to props, similar to this.props === { songs: state.songs }    
+    return { songs: state.songs };
 }
 
 //return connect function, envoke SongList
