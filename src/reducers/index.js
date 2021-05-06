07490e62b140ea62df3fd9d09b09952cca527811
thraddash <theartist@gmail.com>
2021-05-06 00:07:46 -0700

    add song list reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsREducer = () => {
+    return [
+        { title: 'Hey Brother', duration: '4:23' },
+        { title: 'Feel So Close (Nero Remix)', duration: '4:44' },
+        { title: 'The Nights', duration: '3:10' },
+        { title: 'Waiting For Love', duration: '3:10' }
+    ];
+};
